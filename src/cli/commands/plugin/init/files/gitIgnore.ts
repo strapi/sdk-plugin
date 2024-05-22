7d42be83b02bea27396554fb29295c970fbb1982
Ben Irvin <ben@innerdvations.com>
2024-05-22 09:39:39 +0200

    fix: add tsbuildinfo to gitignore (#18)
    * fix: add tsbuildinfo to gitignore
    
    * chore: add changeset
    
    * enhancement: improve gitignore

--- src/cli/commands/plugin/init/files/gitIgnore.ts
+++ src/cli/commands/plugin/init/files/gitIgnore.ts
@@ -7,28 +7,142 @@ const gitIgnoreFile: TemplateFile = {
   contents: outdent`
     # See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
 
-    # dependencies
-    node_modules
-    .pnp
-    .pnp.js
+    ############################
+    # OS X
+    ############################
     
-    # testing
-    coverage
-    
-    # production
-    dist
-    
-    # misc
     .DS_Store
-    *.pem
+    .AppleDouble
+    .LSOverride
+    Icon
+    .Spotlight-V100
+    .Trashes
+    ._*
     
-    # debug
-    npm-debug.log*
-    yarn-debug.log*
-    yarn-error.log*
     
-    # local env files
-    .env    
+    ############################
+    # Linux
+    ############################
+    
+    *~
+    
+    
+    ############################
+    # Windows
+    ############################
+    
+    Thumbs.db
+    ehthumbs.db
+    Desktop.ini
+    $RECYCLE.BIN/
+    *.cab
+    *.msi
+    *.msm
+    *.msp
+    
+    
+    ############################
+    # Packages
+    ############################
+    
+    *.7z
+    *.csv
+    *.dat
+    *.dmg
+    *.gz
+    *.iso
+    *.jar
+    *.rar
+    *.tar
+    *.zip
+    *.com
+    *.class
+    *.dll
+    *.exe
+    *.o
+    *.seed
+    *.so
+    *.swo
+    *.swp
+    *.swn
+    *.swm
+    *.out
+    *.pid
+    
+
+    ############################
+    # Logs and databases
+    ############################
+    
+    .tmp
+    *.log
+    *.sql
+    *.sqlite
+    *.sqlite3
+    
+
+    ############################
+    # Misc.
+    ############################
+    
+    *#
+    ssl
+    .idea
+    nbproject
+    .tsbuildinfo
+    .eslintcache
+    .env
+
+
+    ############################
+    # Strapi
+    ############################
+
+    public/uploads/*
+    !public/uploads/.gitkeep
+
+
+    ############################
+    # Build
+    ############################
+
+    dist
+    build
+    
+
+    ############################
+    # Node.js
+    ############################
+    
+    lib-cov
+    lcov.info
+    pids
+    logs
+    results
+    node_modules
+    .node_history
+    
+
+    ############################
+    # Package managers
+    ############################
+    
+    .yarn/*
+    !.yarn/cache
+    !.yarn/unplugged
+    !.yarn/patches
+    !.yarn/releases
+    !.yarn/sdks
+    !.yarn/versions
+    .pnp.*
+    yarn-error.log
+    
+    
+    ############################
+    # Tests
+    ############################
+    
+    coverage
     `,
 };
 
