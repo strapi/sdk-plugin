2e1ca56d4a4e9262bf909d7a2da55461663b291b
Ben Irvin <ben@innerdvations.com>
2024-08-08 17:01:49 +0200

    enhancement: make path required

--- src/cli/commands/plugin/init/command.ts
+++ src/cli/commands/plugin/init/command.ts
@@ -9,7 +9,7 @@ const command: StrapiCommand = ({ command: commanderCommand, ctx }) => {
   commanderCommand
     .command('init')
     .description('Create a new plugin at a given path')
-    .argument('[path]', 'path to the plugin', './src/plugins/my-plugin')
+    .argument('path', 'path to the plugin')
     .option('-d, --debug', 'Enable debugging mode with verbose logs', false)
     .option('--silent', "Don't log anything", false)
     .action((path, options) => {
