f5ddf691a8be96e008c42dc238650a95ab9ff2cb
Ben Irvin <ben@innerdvations.com>
2024-08-14 10:46:39 +0200

    feat: in Strapi project, init creates plugin in plugin path and logs config info (#36)
    Co-authored-by: Convly <jean-sebastien.herbaux@epitech.eu>

---
+++ .changeset/wet-files-repeat.md
@@ -0,0 +1,5 @@
+---
+'@strapi/sdk-plugin': minor
+---
+
+In Strapi projects, generate plugin in plugins path and log config info
--- src/cli/commands/utils/helpers.ts
+++ src/cli/commands/utils/helpers.ts
@@ -1,6 +1,10 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
+
 import type { CLIContext } from '../../../types';
 
-const runAction =
+export const runAction =
   (name: string, action: (...args: any[]) => Promise<void>) =>
   (ctx: CLIContext, ...args: unknown[]) => {
     const { logger } = ctx;
@@ -14,4 +18,40 @@ const runAction =
       });
   };
 
-export { runAction };
+export const dirContainsStrapiProject = (dir: string) => {
+  try {
+    const packageJsonPath = path.join(dir, 'package.json');
+    const pkgJSON = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+    return Boolean(
+      pkgJSON.dependencies?.['@strapi/strapi'] || pkgJSON.devDependencies?.['@strapi/strapi']
+    );
+  } catch (err) {
+    return false;
+  }
+};
+
+export const logInstructions = (
+  pluginName: string,
+  { language, path: pluginPath }: { language: string; path: string }
+) => {
+  const maxLength = `    resolve: './src/plugins/${pluginName}'`.length;
+  const separator = Array(maxLength).fill('─').join('');
+
+  const exportInstruction = language === 'js' ? 'module.exports =' : 'export default';
+
+  return `
+You can now enable your plugin by adding the following in ${chalk.yellow(
+    `./config/plugins.${language}`
+  )}
+${separator}
+${exportInstruction} {
+  ${chalk.gray('// ...')}
+  ${chalk.green(`'${pluginName}'`)}: {
+    enabled: ${chalk.yellow(true)},
+    resolve: '${chalk.yellow(pluginPath)}'
+  },
+  ${chalk.gray('// ...')}
+}
+${separator}
+`;
+};
